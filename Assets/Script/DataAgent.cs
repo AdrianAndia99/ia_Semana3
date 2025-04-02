037d427e25a2494b94647c04cd65154c0ee05913
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-02 11:54:56 -0500

    kgue risa

--- Assets/Script/DataAgent.cs
+++ Assets/Script/DataAgent.cs
@@ -23,7 +23,6 @@ public class DataAgent : MonoBehaviour
     Coroutine CoroutineEnergy=null;
 
     public bool CantLoadEnergy { get => CoroutineEnergy == null; }
-    // Start is called before the first frame update
     void Start()
     {
         
--- Assets/Script/State/Jugar.cs
+++ Assets/Script/State/Jugar.cs
@@ -23,7 +23,7 @@ public class Jugar : Humano
     {
         if (_DataAgent.Energy.value < 0.25f)
         {
-            _StateMachine.ChangeState(TypeState.Comer);
+            _StateMachine.ChangeState(TypeState.Dormir);
         }
         else 
         {
